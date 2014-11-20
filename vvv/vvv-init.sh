6e2704f853ce38d90a9568442979425abd2a1fbd
andrezrv <andrezrv@gmail.com>
2014-11-20 12:12:07 -0300

    Modify some files get the correct setup.

--- vvv/vvv-init.sh
+++ vvv/vvv-init.sh
@@ -1,4 +1,3 @@
 # Make a database, if we don't already have one
 echo "Creating database (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_dev"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_dev.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS forofyl_wp"
